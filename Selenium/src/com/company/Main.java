782e35d1684db944a065734d3a9e94415ab6902e
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-09-29 12:52:31 +0200

    first commit

---
+++ Selenium/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      /*
+        System.setProperty("webdriver.chrome.driver","C:/Users/mario/Javamapp/18TestAutomation/selenium_jar_and_drivers/chrome_drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.gazzetta.it");
+
+        driver.close();
+
+       */
+
+        System.setProperty("webdriver.gecko.driver","C:/Users/mario/Javamapp/18TestAutomation/selenium_jar_and_drivers/firefox_drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.gazzetta.it");
+
+        driver.close();
+    }
+}
